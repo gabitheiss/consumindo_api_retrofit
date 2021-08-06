75cae4996f69d57fb5c68dbd0d97660aa536aaa5
gabitheiss <t_gaaby@hotmail.com>
2021-08-06 13:39:43 -0300

    TextView devidamente preenchido com dados recebidos da API

--- app/src/main/java/com/example/exercicioretrofit/MainActivity.kt
+++ app/src/main/java/com/example/exercicioretrofit/MainActivity.kt
@@ -27,7 +27,17 @@ class MainActivity : AppCompatActivity(), Callback<Usuarios> {
        response.body()?.apply {
            findViewById<TextView>(R.id.idId).text = this.idUser.toString()
            findViewById<TextView>(R.id.idEmail).text = this.email
-
+           findViewById<TextView>(R.id.idUsername).text = this.username
+           findViewById<TextView>(R.id.idPassword).text = this.password
+           findViewById<TextView>(R.id.idFirstname).text = this.name.firstname
+           findViewById<TextView>(R.id.idLastname).text = this.name.lastname
+           findViewById<TextView>(R.id.idCity).text = this.address.city
+           findViewById<TextView>(R.id.idStreet).text = this.address.street
+           findViewById<TextView>(R.id.idNumber).text = this.address.number.toString()
+           findViewById<TextView>(R.id.idZipcode).text = this.address.zipcode
+           findViewById<TextView>(R.id.idLatitude).text = this.address.geolocation.lat
+           findViewById<TextView>(R.id.idLongitude).text = this.address.geolocation.long
+           findViewById<TextView>(R.id.idPhone).text = this.phone
        }
     }
 
